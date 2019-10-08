7b77b2234382f83eebb381ae7f745eeac32e9bb2
Kevin <ds934118@gmail.com>
2019-10-08 16:00:32 +0800

    Bug fixed

--- adversarial.py
+++ adversarial.py
@@ -18,7 +18,7 @@ iter_num_LL = 7
 iter_num_FGSM = 7
 edit_point_num_LL = 3
 edit_point_num_FGSM = 2
-target_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target_nums = [0]
 momentum = 0.9
 count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -32,7 +32,7 @@ save_pics = True
 use_cuda = True
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-model_url = './trained_models/VGG19_Retrained.pth'
+model_url = './trained_models/VGG19_Strong.pth'
 
 unnorm = UnNormalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
 # Transform
@@ -44,14 +44,14 @@ transform_train = transforms.Compose([
 # Dataloader
 if dataset == 'CIFAR10':
     # Test unseen
-    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=shuffle, num_workers=8)
+    # test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_train)
+    # test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=shuffle, num_workers=8)
 
     # 首次攻擊
-    # train_path = "./Clean_CIFAR10_For_Adv/TrainSet/"
-    # trainset = torchvision.datasets.ImageFolder(train_path, transform=transform_train)
-    # train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=8,
-    #                                           pin_memory=True)
+    train_path = "./Clean_CIFAR10_For_Adv/TrainSet/"
+    trainset = torchvision.datasets.ImageFolder(train_path, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=8,
+                                               pin_memory=True)
 
 # Model
 if dataset == 'CIFAR10':
@@ -78,10 +78,15 @@ for num in range(0, 10):
 
 
 def main():
+    global iter_num_FGSM
+    global iter_num_LL
     model.eval()
     # testing
     for target_num in target_nums:
-        test(model, device, test_loader, epsilons, target_num)
+        for i in range(3, 10):
+            iter_num_LL = i
+            iter_num_FGSM = i
+            test(model, device, train_loader, epsilons, target_num)
 
 
 def test(model, device, test_loader, epsilon, target_num):
@@ -115,8 +120,51 @@ def test(model, device, test_loader, epsilon, target_num):
 
         image_tensor = data.data.clone()
 
-        data = iter_ST_Attack(data, target_fake, image_tensor, epsilon)
-        data = BIM_Attack(data, target_fake, target)
+        # data = iter_ST_Attack(data, target_fake, image_tensor, epsilon)
+        # data = BIM_Attack(data, target_fake, target)
+
+        topk_index = []
+        topk_index2 = []
+        image_tensor = data.data.clone()
+
+        for i in range(0, iter_num_LL):
+            zero_gradients(data)
+            output = model(data)
+            pred = output.max(1, keepdim=True)[1]
+            if pred == target_fake:
+                break
+            loss = F.nll_loss(output, target_fake)
+            loss.backward()
+
+            data_grad = data.grad.data
+
+            # Top K
+            if i == 0:
+                data_grad_r = data_grad.clone().reshape(-1)
+                data_grad_abs = torch.abs(data_grad_r)
+                topk = torch.topk(data_grad_abs, edit_point_num_LL)
+                topk_index = topk[1]
+            data.data = sourceTargetingAttack_topK(data, data_grad, image_tensor, topk_index, epsilon)
+
+        for i in range(0, iter_num_FGSM):
+            zero_gradients(data)
+            output = model(data)
+            pred = output.max(1, keepdim=True)[1]
+            if pred == target_fake:
+                break
+            loss = F.nll_loss(output, target)
+            loss.backward()
+
+            data_grad = data.grad.data
+
+            # Top K
+            if i == 0:
+                data_grad_r = data_grad.clone().reshape(-1)
+                data_grad_abs = torch.abs(data_grad_r)
+                topk = torch.topk(data_grad_abs, edit_point_num_FGSM)
+                topk_index2 = topk[1]
+            data.data = fgsmAttack_topK(data, data_grad, topk_index2)
+
 
         # Check for success
         output = model(data)
@@ -125,9 +173,10 @@ def test(model, device, test_loader, epsilon, target_num):
             correct += 1
         elif final_pred.item() == target_fake.item():
             adv_success += 1
-            print(step, "+1")
+            # print(step, "+1")
             if save_pics:
-                name = "./Adv_CIFAR10/" + labels[target.item()] + "/" + "batch" + str(step) + "_" + labels[
+                name = "./Adv_CIFAR10/" + labels[target.item()] + "/" + "batch" + str(step) + "_Iternum_" + str(
+                    iter_num_LL) + "_" + labels[
                     target.item()] + "To" + \
                        labels[target_fake.item()] + ".png"
                 torchvision.utils.save_image(unnorm(data), filename=name)
@@ -150,58 +199,14 @@ def test(model, device, test_loader, epsilon, target_num):
     print("Incorrect = {} / {} = {:.2%}".format(incorrect, allnum - org_incorrect, final_incorrect))
     print("Successful Adv source-targeting = {} / {} = {:.2%}".format(adv_success, allnum - org_incorrect,
                                                                       final_adv_suc))
-    print("Cost time : {:.2f} seconds".format(tend - tstart))
+    print("Cost time : {:.2f} seconds".format((tend - tstart) / allnum))
     print("")
 
-def iter_ST_Attack(data, target_fake, image_tensor, epsilon):
-    for i in range(0, iter_num_LL):
-        zero_gradients(data)
-        output = model(data)
-        pred = output.max(1, keepdim=True)[1]
-        if pred == target_fake:
-            break
-        loss = F.nll_loss(output, target_fake)
-        loss.backward()
 
-        data_grad = data.grad.data
-        topk_index = []
-        # Top K
-        if i == 0:
-            data_grad_r = data_grad.clone().reshape(-1)
-            data_grad_abs = torch.abs(data_grad_r)
-            topk = torch.topk(data_grad_abs, edit_point_num_LL)
-            topk_index = topk[1]
-
-        data.data = sourceTargetingAttack_topK(data, data_grad, image_tensor, topk_index, epsilon)
-    return data
-
-def BIM_Attack(data, target_fake, target):
-    for i in range(0, iter_num_FGSM):
-        zero_gradients(data)
-        output = model(data)
-        pred = output.max(1, keepdim=True)[1]
-        if pred == target_fake:
-            break
-        loss = F.nll_loss(output, target)
-        loss.backward()
-
-        data_grad = data.grad.data
-        topk_index = []
-        # Top K
-        if i == 0:
-            data_grad_r = data_grad.clone().reshape(-1)
-            data_grad_abs = torch.abs(data_grad_r)
-            topk = torch.topk(data_grad_abs, edit_point_num_FGSM)
-            topk_index = topk[1]
-
-        data.data = fgsmAttack_topK(data, data_grad, topk_index)
-    return data
-
-
-def sourceTargetingAttack_topK(image, data_grad, image_tensor, topk_index, epsilon):
+def sourceTargetingAttack_topK(data, data_grad, image_tensor, topk_index, epsilon):
     sign_data_grad = data_grad.sign()
 
-    perturbed_image = image.clone()
+    perturbed_image = data.clone()
     g = torch.zeros(perturbed_image.size()).to(device)
     v = 0
     for i in range(0, len(topk_index)):
@@ -215,15 +220,15 @@ def sourceTargetingAttack_topK(image, data_grad, image_tensor, topk_index, epsil
     perturbed_image.data = perturbed_image.data + g
     total_grad = perturbed_image - image_tensor
     total_grad = torch.clamp(total_grad, -epsilon, epsilon)
-    adv = image_tensor + total_grad
+    data = image_tensor + total_grad
 
-    return adv
+    return data
 
 
-def fgsmAttack_topK(image, data_grad, topk_index):
+def fgsmAttack_topK(data, data_grad, topk_index):
     sign_data_grad = data_grad.sign()
 
-    perturbed_image = image.clone()
+    perturbed_image = data.clone()
     g = torch.zeros(perturbed_image.size()).to(device)
     v = 0
     for i in range(0, len(topk_index)):
